85de8ae34be8818a971ca085c54bd1d6345f014b
Adam Gutonski <34498995+adamgutons@users.noreply.github.com>
2024-01-19 15:41:09 -0500

    add dockerfile, clean up security config, remove h2 for deployment (#8)

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,23 +2,14 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-import tacos.User;
-import tacos.data.UserRepository;
 
-import static java.util.Objects.nonNull;
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
@@ -37,12 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity, MvcRequestMatcher.Builder mvc) throws Exception {
         httpSecurity
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(toH2Console())     // need to remove h2console settings for deployment
-                        .disable()
-                )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(toH2Console()).permitAll()
                         .requestMatchers(antMatcher("/design"), antMatcher("/orders")).hasRole("USER")
                         .requestMatchers(mvc.pattern("/"), mvc.pattern("/**")).permitAll()
                         .anyRequest().authenticated()
